04e7f7969d8616f8c10006d4e53caef4dff9ff53
Hoang Kien <kienhg96@gmail.com>
2018-03-22 11:36:28 +0700

    Add Map

--- client_driver/android/app/src/main/java/com/client_driver/MainApplication.java
+++ client_driver/android/app/src/main/java/com/client_driver/MainApplication.java
@@ -3,6 +3,7 @@ package com.client_driver;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.corbt.keepawake.KCKeepAwakePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new KCKeepAwakePackage(),
           new MapsPackage()
       );
     }
